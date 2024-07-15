f5ebeeb9ad90eeb8b35be88993e241d95b836b71
junwon-hwnag <nyong1320@gmail.com>
2024-07-15 11:03:07 +0900

    # 10. 스프링 시큐리티 인증 인가 설정

--- src/main/java/com/study/event/api/config/SecurityConfig.java
+++ src/main/java/com/study/event/api/config/SecurityConfig.java
@@ -1,30 +1,31 @@
 package com.study.event.api.config;
 
-// 스프링 시큐리티 설정 파일
-// 인터셉터, 필터 처리
-// 세션검증, 토큰인증
-// 권한처리
-// OAuth2 - SNS로그인
-
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+// 스프링 시큐리티 설정 파일
+// 인터셉터, 필터 처리
+// 세션인증, 토큰인증
+// 권한처리
+// OAuth2 - SNS로그인
 @EnableWebSecurity
 public class SecurityConfig {
 
     // 비밀번호 암호화 객체 컨테이너에 등록 (스프링에게 주입받는 설정)
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    // 시큐리티 설정 ( 스트링 부트 2.7버전 이전 인터페이스를 통해 오버라이딩)
+    // 시큐리티 설정 (스프링 부트 2.7버전 이전 인터페이스를 통해 오버라이딩)
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
 
         http
                 .cors()
@@ -32,11 +33,23 @@ public class SecurityConfig {
                 .csrf().disable() // 필터설정 off
                 .httpBasic().disable() // 베이직 인증 off
                 .formLogin().disable() // 로그인창 off
+                // 세션 인증은 더 이상 사용하지 않음
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests() // 요청 별로 인가 설정
-                .antMatchers("/**").permitAll() // 인가 설정 off
+
+                // 아래의 URL요청은 로그인 없이 모두 허용
+                .antMatchers("/", "/auth/**").permitAll()
+//                .antMatchers(HttpMethod.POST,"/events/**").permitAll()
+
+                // 나머지 요청은 전부 인증(로그인) 후 진행해라
+                .anyRequest().authenticated() // 인가 설정 on
         ;
 
         return http.build();
     }
+
+
 
 }
